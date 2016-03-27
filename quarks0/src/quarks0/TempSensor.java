3fd8cfbac3a0df469bd3c2d7eeb3570f88a890f4
akbence007 <akbence007@gmail.com>
2016-03-27 12:51:23 +0200

    TempSensor set up

--- quarks0/src/quarks0/TempSensor.java
+++ quarks0/src/quarks0/TempSensor.java
@@ -1,5 +1,25 @@
 package quarks0;
 
-public class TempSensor {
+import java.util.Random;
 
+import quarks.function.Supplier;
+
+/**
+ * Every time get() is called, TempSensor generates a temperature reading.
+ */
+public class TempSensor implements Supplier<Double> {
+    double currentTemp = 65.0;
+    Random rand;
+
+    TempSensor(){
+        rand = new Random();
+    }
+
+    @Override
+    public Double get() {
+        // Change the current temperature some random amount
+        double newTemp = rand.nextGaussian() + currentTemp;
+        currentTemp = newTemp;
+        return currentTemp;
+    }
 }
